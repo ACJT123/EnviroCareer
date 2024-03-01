f7fcf7d3375e2e2d7e7623c379f958a1c83deb75
alex <alexcheng@vvinners.com>
2024-03-01 15:42:04 +0800

    complete ui

--- lib/sign_up_page.dart
+++ lib/sign_up_page.dart
@@ -1,4 +1,5 @@
 import 'package:envirocareer/login_page.dart';
+import 'package:envirocareer/widgets/custom_button.dart';
 import 'package:envirocareer/widgets/input_field.dart';
 import 'package:envirocareer/widgets/text_link_button.dart';
 import 'package:flutter/material.dart';
@@ -11,52 +12,155 @@ class SignUpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        alignment: Alignment.topCenter,
-        padding: const EdgeInsets.only(top: 50),
-        child: Column(
-          children: [
-            EarthImage(),
-            const SizedBox(height: 10),
-            HeaderText('EnviroCareer'),
-            const SizedBox(height: 40),
-            const Text(
-              'Sign In',
-              style: TextStyle(
-                fontFamily: 'Roboto',
-                fontSize: 22,
-                fontWeight: FontWeight.w700,
+      body: SingleChildScrollView(
+        child: Container(
+          alignment: Alignment.topCenter,
+          padding: const EdgeInsets.only(top: 50),
+          child: Column(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+                child: Container(
+                  padding: const EdgeInsets.only(left: 30),
+                  alignment: Alignment.topLeft,
+                  child: Icon(Icons.arrow_back),
+                ),
               ),
-            ),
-            const SizedBox(height: 20),
-            InputField(
-              hintText: 'Enter email address',
-              prefixIcon: Icon(Icons.email),
-              inputType: 'email',
-            ),
-            InputField(
-              hintText: 'Enter password',
-              inputType: 'password',
-            ),
-            TextLinkButton(
-              text: 'Forgot Password?',
-              onPressedCallback: () => {
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (BuildContext context) {
-                    return const LoginPage();
-                  }),
-                )
-              },
-              style: GoogleFonts.getFont(
-                'Roboto',
-                fontSize: 15,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
+
+              const Text(
+                'Create Account',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontSize: 22,
+                  fontWeight: FontWeight.w700,
+                ),
               ),
-            )
-          ],
+              const Text(
+                'To get more exclusive contents',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontSize: 12,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+              const SizedBox(height: 20),
+              CreateAccountForm(),
+              // TextLinkButton(
+              //   text: 'Forgot Password?',
+              //   onPressedCallback: () => {
+              //     Navigator.of(context).push(
+              //       MaterialPageRoute(builder: (BuildContext context) {
+              //         return const LoginPage();
+              //       }),
+              //     )
+              //   },
+              //   style: GoogleFonts.getFont(
+              //     'Roboto',
+              //     fontSize: 15,
+              //     fontWeight: FontWeight.bold,
+              //     color: Colors.black,
+              //   ),
+              // )
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class CreateAccountForm extends StatefulWidget {
+  const CreateAccountForm({super.key});
+
+  @override
+  State<CreateAccountForm> createState() => CreateAccountFormState();
+}
+
+class CreateAccountFormState extends State<CreateAccountForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController phoneNumberController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+
+  void _resetForm() {
+    _formKey.currentState!.reset();
+    usernameController.clear();
+    emailController.clear();
+    phoneNumberController.clear();
+    passwordController.clear();
+    confirmPasswordController.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(children: [
+        const SizedBox(height: 10),
+        InputField(
+          hintText: 'Username',
+          prefixIcon: const Icon(Icons.account_circle_outlined),
+          inputType: 'username',
+          controller: usernameController,
+        ),
+        const SizedBox(height: 10),
+        InputField(
+          hintText: 'Email address',
+          prefixIcon: const Icon(Icons.email),
+          inputType: 'email',
+          controller: emailController,
+        ),
+        const SizedBox(height: 10),
+        InputField(
+          hintText: 'Phone number',
+          prefixIcon: const Icon(Icons.phone),
+          inputType: 'phone number',
+          controller: phoneNumberController,
+        ),
+        const SizedBox(height: 10),
+        InputField(
+          hintText: 'Password',
+          prefixIcon: const Icon(Icons.lock),
+          inputType: 'password',
+          controller: passwordController,
+        ),
+        const SizedBox(height: 10),
+        InputField(
+          hintText: 'Confirm Password',
+          prefixIcon: const Icon(Icons.lock),
+          inputType: 'password',
+          controller: confirmPasswordController,
+        ),
+        const SizedBox(height: 40),
+        CustomButton(
+          backgroundColor: const Color(0xFF98B873),
+          text: 'Create Account',
+          textColor: Colors.white,
+          onPressedCallback: () {
+            if (_formKey.currentState!.validate()) {
+              // Dismiss the keyboard
+              FocusScope.of(context).unfocus();
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Processing Data')),
+              );
+            }
+          },
+        ),
+        const SizedBox(height: 10),
+        CustomButton(
+          backgroundColor: Colors.white,
+          textColor: const Color(0xFF98B873),
+          text: 'Reset',
+          onPressedCallback: () {
+            _resetForm();
+          },
+        )
+      ]),
     );
   }
 }
