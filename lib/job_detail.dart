5b46e25c4a1b1fdfd138348111b597d6d263ec3c
acjta <chengjt-wp20@student.tarc.edu.my>
2024-03-18 22:27:22 +0800

    wip: job detail table border width

--- lib/job_detail.dart
+++ lib/job_detail.dart
@@ -1,11 +1,12 @@
+import 'package:envirocareer/model/job.dart';
 import 'package:flutter/material.dart';
 
 class JobDetail extends StatelessWidget {
-  final String companyLogo;
+  final JobModel job;
 
   const JobDetail({
     Key? key,
-    required this.companyLogo,
+    required this.job,
   }) : super(key: key);
 
   @override
@@ -36,6 +37,15 @@ class JobDetail extends StatelessWidget {
               iconTheme: const IconThemeData(
                 color: Colors.white,
               ),
+              actions: <Widget>[
+                Container(
+                  margin: const EdgeInsets.only(right: 5),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.bookmark_add_outlined),
+                  ),
+                ),
+              ],
             ),
           ),
           Positioned(
@@ -54,9 +64,9 @@ class JobDetail extends StatelessWidget {
             ),
           ),
           Positioned(
-            top: screenHeight * 0.1,
-            width: 100,
-            height: 100,
+            top: screenHeight * 0.09,
+            width: 110,
+            height: 110,
             child: Container(
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(15),
@@ -73,7 +83,7 @@ class JobDetail extends StatelessWidget {
                     shape: BoxShape.circle,
                     // borderRadius: BorderRadius.circular(15),
                     image: DecorationImage(
-                      image: NetworkImage(companyLogo),
+                      image: NetworkImage(job.company_logo),
                       fit: BoxFit.fill,
                     ),
                   ),
@@ -81,6 +91,136 @@ class JobDetail extends StatelessWidget {
               ),
             ),
           ),
+          Positioned(
+            top: screenHeight * 0.25,
+            child: Column(
+              children: [
+                Text(
+                  job.job_title,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20,
+                  ),
+                ),
+                Text(
+                  '${job.company_name}, ${job.location}',
+                  style: const TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(15),
+                          bottomLeft: Radius.circular(15),
+                        ),
+                      ),
+                      width: screenWidth / 3,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Text("Experience"),
+                            Text(
+                              job.experience,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                      ),
+                      width: screenWidth / 4,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Text("Salary"),
+                            Text(
+                              job.salary,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(15),
+                          bottomRight: Radius.circular(15),
+                        ),
+                      ),
+                      width: screenWidth / 3,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Text("Job Tyoe"),
+                            Text(
+                              'Full Time',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  width: screenWidth,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'About this job',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        job.description,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            height: 40,
+            bottom: 30,
+            width: screenWidth * 90 / 100,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                'Apply',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF98B873),
+              ),
+            ),
+          )
         ],
       ),
     );
