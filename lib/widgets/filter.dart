041a12ec908f0549cae872c984c0009b6e8db7f1
alex <alexcheng@vvinners.com>
2024-03-21 12:45:19 +0800

    added clear and apply btn for filter

--- lib/widgets/filter.dart
+++ lib/widgets/filter.dart
@@ -1,5 +1,6 @@
 import 'package:envirocareer/widgets/range_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Filter extends StatelessWidget {
   const Filter({super.key});
@@ -16,11 +17,14 @@ class Filter extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(''),
-              Text(
+              const Text(''),
+              const Text(
                 textAlign: TextAlign.center,
                 'Apply Filter',
-                style: TextStyle(fontSize: 18),
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
               GestureDetector(
                 child: Icon(Icons.close),
@@ -31,10 +35,44 @@ class Filter extends StatelessWidget {
             ],
           ),
           const SizedBox(
-            height: 40,
+            height: 30,
           ),
-          CustomRangeSlider(title: 'salary'),
-          CustomRangeSlider(title: 'experience'),
+          CustomRangeSlider(title: 'salary', min: 0, max: 1000),
+          CustomRangeSlider(title: 'experience', min: 0, max: 5),
+          Spacer(),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: ElevatedButton(
+                  // style: ElevatedButton.styleFrom(
+                  //   backgroundColor: const Color(0xFF98B873),
+                  // ),
+                  child: const Text(
+                    'Clear',
+                    style: TextStyle(
+                      color: Color(0xFF98B873),
+                    ),
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+              const SizedBox(width: 20),
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF98B873),
+                  ),
+                  child: const Text(
+                    'Apply',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
--- lib/widgets/range_slider.dart
+++ lib/widgets/range_slider.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:string_capitalize/string_capitalize.dart';
 
 class CustomRangeSlider extends StatefulWidget {
   final String title;
+  final double min;
+  final double max;
 
   const CustomRangeSlider({
     required this.title,
+    required this.min,
+    required this.max,
     super.key,
   });
 
@@ -13,59 +18,72 @@ class CustomRangeSlider extends StatefulWidget {
 }
 
 class CustomRangeSliderState extends State<CustomRangeSlider> {
-  RangeValues _currentRangeValues = const RangeValues(0, 1000);
+  late RangeValues _currentRangeValues;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentRangeValues = RangeValues(widget.min, widget.max);
+  }
+
 
   @override
   Widget build(BuildContext context) {
     String labelUnit = '';
-    
-    // "\$${_currentRangeValues.start.round().toString()} - \$${_currentRangeValues.end.round().toString()}"
 
-    if (widget.title == 'salary'){
+    String labelMin = _currentRangeValues.start.round().toString();
+    String labelMax = _currentRangeValues.end.round().toString();
 
+    if (widget.title == 'salary') {
+      labelUnit = "\$$labelMin - \$$labelMax";
     }
 
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              widget.title,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
+    if (widget.title == 'experience') {
+      labelUnit = "$labelMin - $labelMax";
+    }
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                widget.title.capitalize(),
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            Text(
-              labelUnit,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
+              Text(
+                labelUnit,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-          ],
-        ),
-        SizedBox(
-          child: SliderTheme(
-            data: SliderThemeData(
-              activeTrackColor: const Color(0xFF98B873).withOpacity(0.7),
-              thumbColor: const Color(0xFF98B873),
-            ),
-            child: RangeSlider(
-              values: _currentRangeValues,
-              max: 1000,
-              labels: RangeLabels(
-                _currentRangeValues.start.round().toString(),
-                _currentRangeValues.end.round().toString(),
+            ],
+          ),
+          SizedBox(
+            child: SliderTheme(
+              data: SliderThemeData(
+                activeTrackColor: const Color(0xFF98B873).withOpacity(0.7),
+                thumbColor: const Color(0xFF98B873),
               ),
-              onChanged: (RangeValues values) {
-                setState(() {
-                  _currentRangeValues = values;
-                });
-              },
+              child: RangeSlider(
+                values: _currentRangeValues,
+                min: widget.min,
+                max: widget.max,
+                labels: RangeLabels(labelMin, labelMax),
+                onChanged: (RangeValues values) {
+                  setState(() {
+                    _currentRangeValues = values;
+                  });
+                },
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
